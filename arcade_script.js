9320b53ee875f198d810682b51821c349676b5a0
The Crow's Den <121766671+thecrowsden@users.noreply.github.com>
2026-04-30 09:06:06 -0400

    finish basic battle system

--- arcade_script.js
+++ arcade_script.js
@@ -1,18 +1,70 @@
-const states = {
-	menu: 0,
-	battle: 1,
-	battle_end: 2
-}
-
 const actions = {
 	attack: 0,
 	block: 1
-}
+};
 
 const specials = {
 	none: 0,
-	gilded: 1
-}
+	gilded: 1,
+	chica: 2,
+	foxy: 3,
+	bonnie: 4
+};
+
+const colors = {
+	normal: "white",
+	crit: "darkorange",
+	miss: "orchid",
+	gilded: "goldenrod"
+};
+
+const div_list = [
+	"div_menu",
+	"div_battle_end_button",
+	"div_battle_enemy",
+	"div_battle_player",
+	"div_battle_buttons",
+	"div_battle_textbox"
+];
+
+const states = {
+	menu: {
+		bg_color: "black",
+		div_menu: true,
+		div_battle_end_button: false,
+		div_battle_enemy: false,
+		div_battle_player: false,
+		div_battle_buttons: false,
+		div_battle_textbox: false
+	},
+	battle: {
+		bg_color: "!AREA",
+		div_menu: false,
+		div_battle_end_button: false,
+		div_battle_enemy: true,
+		div_battle_player: true,
+		div_battle_buttons: true,
+		div_battle_textbox: true
+	},
+	battle_start: {
+		bg_color: "!AREA",
+		div_menu: false,
+		div_battle_end_button: false,
+		div_battle_enemy: true,
+		div_battle_player: true,
+		div_battle_buttons: true,
+		div_battle_textbox: false
+	},
+	battle_end: {
+		bg_color: "!AREA",
+		div_menu: false,
+		div_battle_end_button: true,
+		div_battle_enemy: true,
+		div_battle_player: true,
+		div_battle_buttons: false,
+		div_battle_textbox: true
+	}
+};
 
 const enemies = {
 	c_deer: {
@@ -64,6 +116,7 @@ const enemies = {
 const locations = {
 	wf: {
 		name: "WEST FOREST",
+		color: "saddlebrown",
 		rates: [
 			{
 				id: "c_deer",
@@ -83,10 +136,11 @@ const locations = {
 			}
 		]
 	}
-}
+};
 
 let player = {
-	name: "POPGOES THE WEASEL",
+	name_long: "POPGOES THE WEASEL",
+	name_short: "POPGOES",
 	hp: 30,
 	atk: 4,
 	crit: 10,
@@ -117,30 +171,20 @@ function randbool(random_odds) {
 }
 
 function change_state(new_state) {
-	if (new_state == states.menu) {
-		document.getElementById("div_menu").style.display = "block";
-		document.getElementById("div_battle").style.display = "none";
-		document.getElementById("div_battle_buttons").style.display = "none";
-		document.getElementById("div_battle_end_button").style.display = "none";
-	} else if (new_state == states.battle) {
-		document.getElementById("div_menu").style.display = "none";
-		document.getElementById("div_battle").style.display = "block";
-		document.getElementById("div_battle_buttons").style.display = "block";
-		document.getElementById("div_battle_end_button").style.display = "none";
-	} else if (new_state == states.battle_end) {
-		document.getElementById("div_menu").style.display = "none";
-		document.getElementById("div_battle").style.display = "block";
-		document.getElementById("div_battle_buttons").style.display = "none";
-		document.getElementById("div_battle_end_button").style.display = "block";
+	div_list.forEach((i) => document.getElementById(i).style.display = new_state[i] ? "block" : "none");
+	if (new_state.bg_color == "!AREA") {
+		document.body.style.backgroundColor = locations[player.location].color;
+	} else {
+		document.body.style.backgroundColor = new_state.bg_color;
 	}
 }
 
 function display_update() {
 	document.getElementById("text_enemy_name").innerHTML = battle.enemy.name;
-	document.getElementById("text_enemy_name").style.color = battle.enemy.special == specials.gilded ? "gold" : "white";
+	document.getElementById("text_enemy_name").style.color = battle.enemy.special == specials.gilded ? colors.gilded : colors.normal;
 	document.getElementById("text_enemy_hp").innerHTML = "HP: <b>" + battle.enemy_hp + "/" + battle.enemy.hp;
 	document.getElementById("text_enemy_atk").innerHTML = "ATK: <b>" + battle.enemy.atk + "</b>";
-	document.getElementById("text_player_name").innerHTML = player.name;
+	document.getElementById("text_player_name").innerHTML = player.name_long;
 	document.getElementById("text_player_hp").innerHTML = "HP: <b>" + battle.player_hp + "/" + player.hp + "</b>";
 	document.getElementById("text_player_atk").innerHTML = "ATK: <b>" + player.atk + "</b>";
 	document.getElementById("text_output").innerHTML = global_output_text;
@@ -175,45 +219,74 @@ function battle_reset() {
 	battle.player_hp = player.hp;
 	battle.enemy_charging = false;
 	battle.turn_count = 1;
-	change_state(states.battle);
+	change_state(states.battle_start);
 	clear_output_text();
 	display_update();
 }
 
 function turn_hander(player_action) {
 	clear_output_text();
+	change_state(states.battle);
+	let damage_amount = 0;
 	let blocking = false;
 	if (player_action == actions.attack) {
-		output_text("<b>" + player.name + "</b> ATTACKED <b>" + battle.enemy.name + "</b>");
-		let damage_amount = player.atk;
+		output_text("<b>" + player.name_short + "</b> ATTACKED <b>" + battle.enemy.name + "</b>");
+		damage_amount = player.atk;
 		if (randbool(player.crit / 100)) {
 			damage_amount *= 3;
-			output_text("<b>CRIT!</b>");
+			output_text("<b><span style='color: " + colors.crit + ";'>CRIT!</span></b>");
 		}
-		battle.enemy_hp -= player.atk;
-		output_text("<b>" + battle.enemy.name + "</b> TOOK " + damage_amount + " DAMAGE");
-		output_text("");
+		battle.enemy_hp -= damage_amount;
+		output_text("<b>" + battle.enemy.name + "</b> TOOK <b>" + damage_amount + "</b> DAMAGE");
 	}
 	if (player_action == actions.block) {
-		output_text("<b>" + player.name + "</b> BLOCKED");
-		output_text("");
 		blocking = true;
+		output_text("<b>" + player.name_short + "</b> BLOCKED");
 	}
+	output_text("");
 	
 	if (battle.enemy_hp <= 0) {
-		output_text("<b>YOU WIN!</b>");
-		output_text("PRIZE: " + battle.enemy.reward + " COINS");
-		output_text("");
 		battle.enemy_hp = 0;
 		change_state(states.battle_end);
+		output_text("<b>YOU WIN!</b>");
+		output_text("PRIZE: <b>" + battle.enemy.reward + "</b> COINS");
 	} else {
 		/*if (battle.enemy.special == specials.chica && battle.turn_count % 3 == 0) {
 			//boss actions
 		} else {*/
-			//if (randbool())
-	//}
+			if (!battle.enemy_charging && randbool(battle.enemy.crit / 100)) {
+				battle.enemy_charging = true;
+  				output_text("<b>" + battle.enemy.name + "</b> IS <b><span style='color: " + colors.crit + ";'>CHARGING</span></b>...");
+			} else {
+				output_text("<b>" + battle.enemy.name + "</b> ATTACKED <b>" + player.name_short + "</b>");
+				if (randbool(battle.enemy.miss / 100)) {
+					output_text("<b><span style='color: " + colors.miss + ";'>MISS!</span></b>");
+				} else {
+					damage_amount = battle.enemy.atk;
+					if (blocking) {
+						damage_amount /= 2;
+						output_text("<b>BLOCK!</b>");
+					}
+					if (battle.enemy_charging) {
+						damage_amount *= 3;
+						output_text("<b><span style='color: " + colors.crit + ";'>CRIT!</span></b>");
+					}
+					damage_amount = Math.round(damage_amount);
+					battle.player_hp -= damage_amount;
+					output_text("<b>" + player.name_short + "</b> TOOK <b>" + damage_amount + "</b> DAMAGE");
+				}
+				battle.enemy_charging = false;
+			}
+		//}
+		if (battle.player_hp <= 0) {
+			battle.player_hp = 0;
+			change_state(states.battle_end);
+			output_text("");
+			output_text("<b>YOU LOSE...</b>");
+		}
 	}
-		
+	output_text("");
+	
 	battle.turn_count++;
 	output_text("dev: current turn: " + battle.turn_count);
 	display_update();
@@ -229,7 +302,8 @@ function btn_battle_block() {
 
 function btn_battle_info() {
 	clear_output_text();
-	output_text("NAME: <b>" + battle.enemy.name + "</b>");
+	change_state(states.battle);
+	output_text("NAME: <b>" + (battle.enemy.special == specials.gilded ? "<span style='color: " + colors.gilded + ";'>" : "") + battle.enemy.name + (battle.enemy.special == specials.gilded ? "</span>" : "") + "</b>");
 	output_text("LOCATION: <b>" + battle.enemy.location + "</b>");
 	output_text("HP: <b>" + battle.enemy.hp + "</b>");
 	output_text("ATK: <b>" + battle.enemy.atk + "</b>");
@@ -247,4 +321,14 @@ function btn_battle_back() {
 function btn_dev_load_battle() {
 	generate_enemy(player.location);
 	battle_reset();
+}
+
+function btn_dev_min_stats() {
+	player.hp = 30;
+	player.atk = 4;
+}
+
+function btn_dev_max_stats() {
+	player.hp = 300;
+	player.atk = 30;
 }
